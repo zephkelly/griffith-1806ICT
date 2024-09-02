bd1aca0d90bef3f245737446b08719710710c839
zephkelly <masterevank@gmail.com>
2024-09-02 15:09:02 +1000

    Improve file reader by breaking key/value pairs at colon

--- tsp_file_reader.c
+++ tsp_file_reader.c
@@ -7,6 +7,23 @@
 
 #define MAX_LINE_LENGTH 1000
 
+char *trim_whitespace(char *str)
+{
+    char *end;
+
+    while(isspace((unsigned char)*str)) str++;
+
+    if(*str == 0)
+        return str;
+
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) end--;
+
+    end[1] = '\0';
+
+    return str;
+}
+
 void read_tsp_file(const char* filename, TSPProblem* problem)
 {
     FILE* input_file = fopen(filename, "r");
@@ -19,64 +36,56 @@ void read_tsp_file(const char* filename, TSPProblem* problem)
 
     char line[MAX_LINE_LENGTH];
     bool reading_nodes = false;
-    int current_node = 0;
+    int node_count = 0;
 
     while (fgets(line, sizeof(line), input_file))
     {
+        char* formatted_line = trim_whitespace(line);
 
         if (strncmp(line, "EOF", 3) == 0)
         {
             break;
         }
 
-        if (strncmp(line, "NAME :", 6) == 0)
+        char* colon_char = strchr(formatted_line, ':');
+
+        if (colon_char)
         {
-            sscanf(line, "NAME : %s", problem->name);
-        }
-        else if (strncmp(line, "COMMENT :", 9) == 0)
-        {
-            sscanf(line, "COMMENT : %s", problem->comment);
-        }
-        else if (strncmp(line, "TYPE :", 6) == 0)
-        {
-            sscanf(line, "TYPE : %s", problem->type);
-        }
-        else if (strncmp(line, "DIMENSION:", 10) == 0)
-        {
-            sscanf(line, "DIMENSION: %d", &problem->dimension);
-        }
-        else if (strncmp(line, "EDGE_WEIGHT_TYPE :", 17) == 0)
-        {
-            sscanf(line, "EDGE_WEIGHT_TYPE : %s", problem->edge_weight_type);
+            *colon_char = '\0';
+            char* key = trim_whitespace(formatted_line);
+            char* value = trim_whitespace(colon_char + 1);
+
+            if (strncmp(key, "NAME", 4) == 0)
+            {
+                sscanf(value, "%49s", problem->name);
+            }
+            else if (strncmp(key, "COMMENT", 7) == 0)
+            {
+                sscanf(value, "%99s", problem->comment);
+            }
+            else if (strncmp(key, "TYPE", 4) == 0)
+            {
+                sscanf(value, "%19s", problem->type);
+            }
+            else if (strncmp(key, "DIMENSION", 9) == 0)
+            {
+                sscanf(value, "%d", &problem->dimension);
+            }
+            else if (strncmp(key, "EDGE_WEIGHT_TYPE", 16) == 0)
+            {
+                sscanf(value, "%19s", problem->edge_weight_type);
+            }
         }
         else if (strncmp(line, "NODE_COORD_SECTION", 18) == 0)
         {
             reading_nodes = true;
         }
-        else if (reading_nodes && current_node < problem->dimension)
+        else if (reading_nodes && node_count < problem->dimension)
         {
-            current_node++;
+            node_count++;
         }
     }
 
-    problem->read_nodes = current_node;
+    problem->read_nodes = node_count;
     fclose(input_file);
-}
-
-// https://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
-char *trimwhitespace(char *str)
-{
-  char *end;
-
-  while(isspace((unsigned char)*str)) str++;
-
-  if(*str == 0)
-    return str;
-
-  end = str + strlen(str) - 1;
-  while(end > str && isspace((unsigned char)*end)) end--;
-
-  end[1] = '\0';
-
-  return str;
 }
