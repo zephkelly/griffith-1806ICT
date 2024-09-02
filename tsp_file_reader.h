733cb3b302c06143e34e08d8bc8e93e2abffd680
zephkelly <masterevank@gmail.com>
2024-09-02 13:50:04 +1000

    Create tsp file reader header and problem representation

---
+++ tsp_file_reader.h
@@ -0,0 +1,21 @@
+#ifndef TSP_FILE_READER_H
+#define TSP_FILE_READER_H
+
+typedef struct {
+    int id;
+    double x;
+    double y;
+} City;
+
+typedef struct {
+    char name[50];
+    char comment[255];
+    char type[20];
+    int dimension;
+    char edge_weight_type[20];
+    City cities[1000];
+} TSPProblem;
+
+void read_tsp_file(const char* filename, TSPProblem* tsp);
+
+#endif
