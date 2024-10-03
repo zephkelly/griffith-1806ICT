1c62a63d63c56de714c217465b9c1cccbfe5ba2e
zephkelly <masterevank@gmail.com>
2024-10-03 12:08:28 +1000

    Read in binary file and allocate frequency table array

--- huffman.c
+++ huffman.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define FREQ_TABLE_SIZE 128
+#define FREQUENCY_TABLE_SIZE 128
 
-// Define structures
 typedef struct HuffmanNode {
     char character;
     int frequency;
     struct HuffmanNode *left, *right;
 } HuffmanNode;
 
-// Function prototypes
 void read_frequency_table(FILE *file, int *freq_table);
 int read_bitstream_length(FILE *file);
 HuffmanNode* build_huffman_tree(int *freq_table);
@@ -19,6 +17,15 @@ void decode_message(FILE *file, HuffmanNode *root, int bitstream_length);
 
 int main()
 {
+    FILE *file = fopen ("message.bin", "rb");
+
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file: 'message.bin'\n");
+        return 1;
+    }
+
+    int frequency_table[FREQUENCY_TABLE_SIZE];
+
     return 0;
 }
 
